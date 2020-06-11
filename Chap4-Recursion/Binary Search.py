2efe485351e100e4576e81b79b5dbbe659b1013f
hatmahat <56577748+hatmahat@users.noreply.github.com>
2020-06-11 18:47:13 +0700

    Update Binary Search.py

--- Chap4-Recursion/Binary Search.py
+++ Chap4-Recursion/Binary Search.py
@@ -4,16 +4,16 @@ def binary_search(data, target, low, high):
     The search only considers the portion from data[low] to data[high] inclusive.
     """
     if low > high:
-        return False
+        return False            # kondisi akhir
     else:                               # interval is empty; no match
         mid = (low + high) // 2
-        if target == mid:         # found a match
-            return True
+        if target == mid:                # found a match
+            return True         # kondisi akhir
         elif target < mid:
             # recur on the portion left of the middle
-            return binary_search(data, target, low, mid-1)
+            return binary_search(data, target, low, mid-1)      # pengulangan sampai kondisi akhir tercapai
         else:
-            return binary_search(data, target, mid + 1, high)
+            return binary_search(data, target, mid + 1, high)   # pengulangan sampai kondisi akhir tercapai
 
 # TESTING
 arr = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
