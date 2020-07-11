6869cea260722ef4f03cda15f4acbf7a384854f5
hatmahat <56577748+hatmahat@users.noreply.github.com>
2020-07-11 15:35:17 +0700

    Add files via upload

--- codecademy-complex_data_structures/HashMap.py
+++ codecademy-complex_data_structures/HashMap.py
@@ -1,81 +1,41 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
 class HashMap:
-    def __init__(self, array_size):
-      self.array_size = array_size
-      self.array = [None for item in range(array_size)]
+  def __init__(self, size):
+    self.array_size = size
+    self.array = [LinkedList() for item in range(size)]
 
-    def hash(self, key, count_collisions=0):
-      key_bytes = key.encode()
-      hash_code = sum(key_bytes)
-      return hash_code + count_collisions
+  def hash(self, key):
+    return sum(key.encode())
 
-    def compressor(self, hash_code):
-      return hash_code % self.array_size
+  def compress(self, hash_code):
+    return hash_code % self.array_size
 
-    def assign(self, key, value):
-      array_index = self.compressor(self.hash(key))
-      current_array_value = self.array[array_index]
+  def assign(self, key, value):
+    array_index = self.compress(self.hash(key))
+    payload = Node([key, value])
+    list_at_array = self.array[array_index]
 
-      if current_array_value is None:
-        self.array[array_index] = [key, value]
+    for item in list_at_array:
+      if key == item[0]:
+        item[1] = value
         return
 
-      if current_array_value[0] == key:
-        self.array[array_index] = [key, value]
-        return
+    list_at_array.insert(payload)
 
-      # Collision!
+  def retrieve(self, key):
+    array_index = self.compress(self.hash(key))
+    list_at_index = self.array[array_index]
 
-      number_collisions = 1
+    for item in list_at_index:
+      if key == item[0]:
+        return item[1]
+    return 
 
-      while(current_array_value[0] != key):
-        new_hash_code = self.hash(key, number_collisions)
-        new_array_index = self.compressor(new_hash_code)
-        current_array_value = self.array[new_array_index]
+blossom = HashMap(len(flower_definitions))
 
-        if current_array_value is None:
-          self.array[new_array_index] = [key, value]
-          return
+for flower in flower_definitions:
+  blossom.assign(flower[0], flower[1])
 
-        if current_array_value[0] == key:
-          self.array[new_array_index] = [key, value]
-          return
-
-        number_collisions += 1
-
-      return
-
-    def retrieve(self, key):
-      array_index = self.compressor(self.hash(key))
-      possible_return_value = self.array[array_index]
-
-      if possible_return_value is None:
-        return None
-
-      if possible_return_value[0] == key:
-        return possible_return_value[1]
-
-      retrieval_collisions = 1
-
-      while (possible_return_value != key):
-        new_hash_code = self.hash(key, retrieval_collisions)
-        retrieving_array_index = self.compressor(new_hash_code)
-        possible_return_value = self.array[retrieving_array_index]
-
-        if possible_return_value is None:
-          return None
-
-        if possible_return_value[0] == key:
-          return possible_return_value[1]
-
-        retrieval_collisions += 1
-
-      return
-
-hash_map = HashMap(15)
-hash_map.assign('gabbro', 'igneous')
-hash_map.assign('sandstone', 'sedimentary')
-hash_map.assign('gneiss', 'metamorphic')
-
-print(hash_map.retrieve('gabbro'))
-print(hash_map.retrieve('sandstone'))
-print(hash_map.retrieve('gneiss'))
+print(blossom.retrieve('daisy'))
