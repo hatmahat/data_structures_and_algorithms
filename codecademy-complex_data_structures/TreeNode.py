811229d837cd4925bd3361a3f9a03ba75d9b8468
hatmahat <56577748+hatmahat@users.noreply.github.com>
2020-07-12 22:20:18 +0700

    Add files via upload

---
+++ codecademy-complex_data_structures/TreeNode.py
@@ -0,0 +1,24 @@
+class TreeNode:
+  def __init__(self, value):
+    self.value = value # data
+    self.children = [] # reference to other nodes
+
+  def add_child(self, child_node):
+    # creates parent-child relationship
+    print("Adding", child_node.value)
+    self.children.append(child_node)
+
+  def remove_child(self, child_node):
+    # remove parent-child relationship
+    print(
+      "Removing {} from {}".format(child_node.value, self.value)
+      )
+    self.children = [child for child in self.children if child is not child_node]
+
+  def traverse(self):
+    # moves through each node referenced from self downwards
+    nodes_to_visit = [self]
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop()
+      print(current_node.value)
+      nodes_to_visit += current_node.children
